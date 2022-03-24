beefc83318e528f96eb60c512a621f5c9603cf3b
Roberto Rivera <99135260+RobertoRiveraMx@users.noreply.github.com>
2022-03-23 19:46:06 -0600

    Add files via upload

---
+++ PokeApi_launchX_Final/assets/js/pokedexLaunchX.js
@@ -0,0 +1,34 @@
+
+
+const fetchPokemon = () => {
+    const pokeName = document.getElementById("pokeName");
+    let pokeInput = pokeName.value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+    fetch(url).then ((res) => {
+        //console.log(res);
+        if (res.status != "200") {
+            console.log(res);
+            pokeImages("img/NFPokemon.giff");
+        }else{
+            return res.json();
+        }        
+    }).then((data) => {
+        console.log(data);
+        let pokeImg = data.sprites.front_default;
+        console.log(pokeImg);
+        pokeImages(pokeImg);
+    })
+}
+
+//fetchPokemon()
+
+const pokeImages = (url) => {
+    const pokeImg = document.getElementById("pokeImg");
+    pokeImg.src = url;
+}
+
+//pokeImages("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png");
+
+
+
+
